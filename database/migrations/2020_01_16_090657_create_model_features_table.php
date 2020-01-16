9f9bbfcf87e0e61d8582906a57ddfffbd0ee95ce
ZULHAM AZWAR ACHMAD <zulham724@students.unnes.ac.id>
2020-01-16 16:09:21 +0700

    add migration

---
+++ database/migrations/2020_01_16_090657_create_model_features_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateModelFeaturesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('model_features', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('model_features');
+    }
+}
